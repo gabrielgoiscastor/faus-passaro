7521462995a3008d6a65d4bcc8b6114b655f36ef
Gabriel Gois de Melo <gabriel@caseimagine.com.br>
2019-02-10 00:09:55 +0000

    listing...

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [ OfertasService ]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
+
+  private ofertas: Oferta[];
+  
 
   ngOnInit() {
+    this.ofertasService.getOfertasPorCategoria('diversao')
+    .then((resposta: any) => this.ofertas = resposta);
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -11,8 +11,16 @@ export class OfertasService{
 
     }
 
+    private apiUrl: string = 'http://localhost:3000/ofertas/';
+
     public getOfertas(): Promise<Array<Oferta>>{
 
-        return this.http.get('http://localhost:3000/ofertas/?destaque=true').toPromise().then((resposta: any) => resposta.json());
+        return this.http.get(this.apiUrl + '?destaque=true').toPromise()
+        .then((resposta: any) => resposta.json());
+    }
+
+    public getOfertasPorCategoria(categoria: string) : Promise<Array<Oferta>>{
+        return this.http.get(this.apiUrl + `?categoria=${categoria}`).toPromise()
+        .then((resposta: any) => resposta.json());
     }
 }
