d3436a5ee653f529f8bc8383d683a7a4479bab62
Gabriel Gois de Melo <gabriel@caseimagine.com.br>
2019-02-10 00:23:23 +0000

    navegando para ids

---
+++ src/app/oferta/oferta.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
+})
+export class OfertaComponent implements OnInit {
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {
+  }
+
+}
