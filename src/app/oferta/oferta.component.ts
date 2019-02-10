e14b2d021e133e823fd3b8691316e8ee70899f3d
Gabriel Gois de Melo <gabriel@caseimagine.com.br>
2019-02-10 00:49:37 +0000

    URL da API e outras coisas mais

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-oferta',
@@ -10,9 +11,28 @@ import { Oferta } from '../shared/oferta.model';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private ofertasService: OfertasService) { }
+  private ofertaId: number;
+  private oferta: Oferta;
+  private imagemAtual: string;
+
+  constructor(private route: ActivatedRoute, private ofertasService: OfertasService) {
+    this.imagemAtual = "";
+  }
 
   ngOnInit() {
+    //this.ofertaId = this.route.snapshot.params['id'];
+
+    this.route.params.subscribe((parametro: any) => this.ofertaId = parametro.id);
+
+    this.ofertasService.getOfertaPorId(this.ofertaId).then((retorno: Oferta) => {
+      this.oferta = retorno;
+      this.imagemAtual = this.oferta.imagens[0].url;
+    });
+
+  }
+
+  mudarFull(imagem: string){
+    this.imagemAtual = imagem;
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -15,12 +15,19 @@ export class OfertasService{
 
     public getOfertas(): Promise<Array<Oferta>>{
 
-        return this.http.get(this.apiUrl + '?destaque=true').toPromise()
+        return this.http.get(`${this.apiUrl}?destaque=true`).toPromise()
         .then((resposta: any) => resposta.json());
     }
 
     public getOfertasPorCategoria(categoria: string) : Promise<Array<Oferta>>{
-        return this.http.get(this.apiUrl + `?categoria=${categoria}`).toPromise()
+        return this.http.get(`${this.apiUrl}?categoria=${categoria}`).toPromise()
         .then((resposta: any) => resposta.json());
+    }
+
+    public getOfertaPorId(id: number) : Promise<Oferta>{
+
+        return this.http.get(`${this.apiUrl}?id=${id}`).toPromise()
+        .then((resposta: any) => resposta.json()[0]);
+
     }
 }
